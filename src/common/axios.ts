c310eeb5db44d8ec83319796feebae2d782b59bd
Ares <89419093+arestosora@users.noreply.github.com>
2024-07-17 22:20:17 -0500

    fix: removed ununsed axios route

--- src/common/axios.ts
+++ src/common/axios.ts
@@ -1,7 +1,7 @@
 import { authState } from '@/services/auth/auth';
 import axios from 'axios';
 
-axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api/v1`;
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}`;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
